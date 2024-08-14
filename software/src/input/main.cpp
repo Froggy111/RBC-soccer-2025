d617e0259adda3fb28be36773d8342bbbcc2db7b
AJR07 <angjunray@live.com>
2024-08-14 17:55:04 +0800

    :sparkles: Loop works!

--- software/src/input/main.cpp
+++ software/src/input/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <unistd.h>
+
 void InputLoop() {
-	
+	for (int i = 0; i < 1000; i++){
+		sleep(1);
+		std::printf("InputLoop: %d\n", i);
+	}
 }
--- software/utils/vector.hpp
+++ software/utils/vector.hpp
@@ -1,6 +1,6 @@
 #pragma once
-
 #include "types/numbers.hpp"
+
 struct Vector {
 	types::f64 x;
 	types::f64 y;
