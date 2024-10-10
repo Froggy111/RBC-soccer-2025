e25a3cf287f82d8e82e87ba04176b9d64bc08057
AJR07 <angjunray@live.com>
2024-10-10 10:06:26 +0800

    :bug: Fix state output

--- software/middle-plate-rpi/src/main/state_output.cpp
+++ software/middle-plate-rpi/src/main/state_output.cpp
@@ -1,5 +1,6 @@
 #include "state_output.hpp"
 #include <mutex>
+#include "spdlog/spdlog.h"
 
 std::mutex m_out;
 
@@ -17,7 +18,31 @@ void OutputStateManager::update_state()
 {
 	m_out.lock();
 	state->counter++;
+	OutputState copy = *state;
 	m_out.unlock();
+
+	for (std::pair<std::function<void(OutputState)>, int> funcInfo : functions)
+	{
+		if (!funcInfo.second) continue;
+		std::function<void(OutputState)> func = funcInfo.first;
+		func(copy);
+	}
 }
 
-OutputStateManager output_state_manager = OutputStateManager();
+int OutputStateManager::add_function(std::function<void(OutputState)> func)
+{
+	int pos = functions.size();
+	functions.push_back({
+		func, true
+	});
+
+	// returns function position
+	return pos;
+}
+
+void OutputStateManager::deactivate_fuction(int pos)
+{
+	functions[pos].second = false;
+}
+
+OutputStateManager input_state_manager = OutputStateManager();
