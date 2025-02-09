c5f33e4252e1152666668c3ed31c2bacd27dab57
vs358 <sriramvenky24@gmail.com>
2025-02-09 21:10:17 +0800

    Changes to be committed: 	modified:   software/bottom/libs/digital-mux/pinmap.hpp

--- software/bottom/libs/digital-mux/pinmap.hpp
+++ software/bottom/libs/digital-mux/pinmap.hpp
@@ -11,29 +11,29 @@ enum pinmap{
 };
 
 enum mux1a{
-    mux1a_SCLK = 11,
-    mux1a_MOSI = 12,
-    mux1a_MISO = 13,
+    mux1a_SCLK,
+    mux1a_MOSI,
+    mux1a_MISO,
     mux1a_CS = 14
 };
 
 enum mux1b{
-    mux1b_SCLK = 11,
-    mux1b_MOSI = 12,
-    mux1b_MISO = 13,
+    mux1b_SCLK,
+    mux1b_MOSI,
+    mux1b_MISO,
     mux1b_CS = 14
 };
 
 enum mux2a{
-    mux2a_SCLK = 11,
-    mux2a_MOSI = 12,
-    mux2a_MISO = 13,
+    mux2a_SCLK,
+    mux2a_MOSI,
+    mux2a_MISO,
     mux2a_CS = 20
 };
 
 enum mux2b{
-    mux2b_SCLK = 11,
-    mux2b_MOSI = 12,
-    mux2b_MISO = 13,
+    mux2b_SCLK,
+    mux2b_MOSI,
+    mux2b_MISO,
     mux2b_CS = 14
 };
