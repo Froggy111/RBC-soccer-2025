a728db642d64831ab4b2c959baf0f7b5d87bc47b
Froggy111 <lijiayi4789@gmail.com>
2025-02-10 20:37:37 +0800

    fixed naming of pinmap

--- software/bottom/libs/hardware-descriptors/pinmap.hpp
+++ software/bottom/libs/hardware-descriptors/pinmap.hpp
@@ -2,7 +2,7 @@
 
 namespace pinmap {
 
-enum class pins : types::u8 {
+enum class Pins : types::u8 {
   DRV2_IN1 = 0,
   DRV2_IN2 = 1,
   SPI0_SCLK = 2,
@@ -30,7 +30,7 @@ enum class pins : types::u8 {
   AMUX2_COM = 27,
   AMUX1_COM = 28,
 
-  // mux1a
+  // Mux1A
   DRV4_OFF = 29,
   MOUSE1_RST = 30,
   MOUSE1_SCS = 31,
@@ -40,7 +40,7 @@ enum class pins : types::u8 {
   AMUX_S1 = 35,
   AMUX_S0 = 36,
 
-  // mux1b
+  // Mux1B
   DRV1_OFF = 37,
   DRV1_SCS = 38,
   DRV1_NSLEEP = 39,
@@ -50,7 +50,7 @@ enum class pins : types::u8 {
   DRV4_NSLEEP = 43,
   DRV4_SCS = 44,
 
-  // mux2a
+  // Mux2A
   DRV2_NFAULT = 45,
   DRV2_NSLEEP = 46,
   MOUSE2_RST = 47,
@@ -60,7 +60,7 @@ enum class pins : types::u8 {
   DRV3_NSLEEP = 51,
   DRV3_NFAULT = 52,
 
-  // mux2b
+  // Mux2B
   DRV5_NFAULT = 53,
   ADC1_INT = 54,
   ADC2_INT = 55,
@@ -70,20 +70,20 @@ enum class pins : types::u8 {
   DRV2_OFF = 59,
   DRV2_SCS = 60,
 
-  // adc1
+  // ADC1
   DRV2_IPROPI = 61,
   LIGHTGATE = 62,
   DRV3_IPROPI = 63,
   DRV5_IPROPI = 64,
 
-  // adc2
+  // ADC2
   LOADCELL_AMINUS = 65,
   LOADCELL_APLUS = 66,
   DRV1_IPROPI = 67,
   DRV4_IPROPI = 68
 };
 
-enum class pico : types::u8 {
+enum class Pico : types::u8 {
   DRV2_IN1 = 0,
   DRV2_IN2 = 1,
   SPI0_SCLK = 2,
@@ -112,7 +112,7 @@ enum class pico : types::u8 {
   AMUX1_COM = 28
 };
 
-enum class mux1a : types::u8 {
+enum class Mux1A : types::u8 {
   DRV4_OFF = 0,
   MOUSE1_RST = 1,
   MOUSE1_SCS = 2,
@@ -123,7 +123,7 @@ enum class mux1a : types::u8 {
   AMUX_S0 = 7
 };
 
-enum class mux1b : types::u8 {
+enum class Mux1B : types::u8 {
   DRV1_OFF = 0,
   DRV1_SCS = 1,
   DRV1_NSLEEP = 2,
@@ -134,7 +134,7 @@ enum class mux1b : types::u8 {
   DRV4_SCS = 7
 };
 
-enum class mux2a : types::u8 {
+enum class Mux2A : types::u8 {
   DRV2_NFAULT = 0,
   DRV2_NSLEEP = 1,
   MOUSE2_RST = 2,
@@ -145,7 +145,7 @@ enum class mux2a : types::u8 {
   DRV3_NFAULT = 7
 };
 
-enum class mux2b : types::u8 {
+enum class Mux2B : types::u8 {
   DRV5_NFAULT = 0,
   ADC1_INT = 1,
   ADC2_INT = 2,
@@ -156,14 +156,14 @@ enum class mux2b : types::u8 {
   DRV2_SCS = 7
 };
 
-enum class adc1 : types::u8 {
+enum class ADC1 : types::u8 {
   DRV2_IPROPI = 0,
   LIGHTGATE = 1,
   DRV3_IPROPI = 2,
   DRV5_IPROPI = 3
 };
 
-enum class adc2 : types::u8 {
+enum class ADC2 : types::u8 {
   LOADCELL_AMINUS = 0,
   LOADCELL_APLUS = 1,
   DRV1_IPROPI = 2,
