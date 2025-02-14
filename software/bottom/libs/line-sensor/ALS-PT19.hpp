cd776f9db2c9baadd5c08b036cdb61da92cc50ed
jingyi679 <j29269428@gmail.com>
2025-02-14 19:47:30 +0800

    hpp for linesensor

---
+++ software/bottom/libs/line-sensor/ALS-PT19.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <Wire.h> // for I2C??
+#define LIGHT_SENSOR_HPP
+#define LIGHT_SENSOR_ADDR 0x39 // I2C address of ALS-PT19
+
+class LightSensor {
+    public:
+        LightSensor();
+        bool begin();
+        void read();
+        void set_integration_time(uint16_t time);
+        void set_gain(uint8_t gain);
+    private:
+        uint16_t get_lux();
+        uint16_t get_raw();
+        uint16_t get_integration_time();
+        uint8_t get_gain();
+
+        void write_register(uint8_t reg, uint8_t val);
+        uint8_t read_register(uint8_t reg);
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
