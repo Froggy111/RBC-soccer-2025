aed931daac0d1685f63d4df663d3d65b819f3878
jingyi679 <j29269428@gmail.com>
2025-03-07 18:56:35 +0800

    updated linesensor init

--- software/bottom/libs/line-sensor/ALSPT19.cpp
+++ software/bottom/libs/line-sensor/ALSPT19.cpp
@@ -12,24 +12,24 @@ extern "C" {
 void LineSensor::init(types::u8 id, spi_inst_t *spi_obj) {
     printf("---> Initializing ALSPT19\n");
     
+    //init dmux
     dmux.init(1, spi_obj);
 
+    //init dmux gpio pins
+    dmux.init_gpio((int) pinmap::Mux1A::AMUX_S0, true, true);
+    dmux.init_gpio((int) pinmap::Mux1A::AMUX_S1, true, true);
+    dmux.init_gpio((int) pinmap::Mux1A::AMUX_S2, true, true);
+    dmux.init_gpio((int) pinmap::Mux1A::AMUX_S3, true, true);
+    dmux.init_gpio((int) pinmap::Mux1A::AMUX_EN, true, true);
+
+    //init adc
     adc_init();  // initialise ADC 
-    adc_gpio_init((int) pinmap::DigitalPins::AMUX_S2); // initialise ADC pin
-    adc_gpio_init((int) pinmap::DigitalPins::AMUX_S1); // initialise ADC pin
-    adc_gpio_init((int) pinmap::DigitalPins::AMUX_S3); // initialise ADC pin
-    
-    
+    adc_gpio_init((int) pinmap::Pico::AMUX1_COM); 
+    adc_gpio_init((int) pinmap::Pico::AMUX2_COM); 
+    adc_gpio_init((int) pinmap::Pico::AMUX3_COM);
 }
 
 void LineSensor::select_channel(uint8_t channel) {
-    MCP23S17.init_gpio(GPA7, true, true);
-    MCP23S17.init_gpio(GPA6, true, true);
-    MCP23S17.init_gpio(GPA4, true, true);
-    MCP23S17.init_gpio(GPA3, true, true);
-    MCP23S17.write_gpio(GPA7, true, true);
-    MCP23S17.write_gpio(GPA6, true, true);
-    MCP23S17.write_gpio(GPA5, true, true);
     MCP23S17.write_gpio(s3_pin, true, true); //selects which amux channel to read from
 }
 
