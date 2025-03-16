d9a352e3fe74dd6e5af27677a0e35da8122e2697
Froggy111 <lijiayi4789@gmail.com>
2025-03-16 12:46:06 +0800

    changed usb hello world test to use global cdc object to avoid confusion

--- software/bottom/test/comms/usb-hello-world/main.cpp
+++ software/bottom/test/comms/usb-hello-world/main.cpp
@@ -6,12 +6,11 @@ using namespace types;
 const u8 LED_PIN = 25;
 
 void hello_world_task(void *args) {
-  usb::CDC *cdc = (usb::CDC *)args;
   char payload[] = "Hello world!\n";
   while (true) {
     gpio_put(LED_PIN, 0);
     sleep_ms(500);
-    cdc->printf(payload);
+    comms::USB_CDC.printf(payload);
     gpio_put(LED_PIN, 1);
     sleep_ms(500);
   }
@@ -21,10 +20,9 @@ int main() {
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
   gpio_put(LED_PIN, 1);
-  usb::CDC cdc = usb::CDC();
-  cdc.init();
-  xTaskCreate(hello_world_task, "hello_world_task", 1024, &cdc, 10, NULL);
-  
+  comms::USB_CDC.init();
+  xTaskCreate(hello_world_task, "hello_world_task", 1024, NULL, 10, NULL);
+
   vTaskStartScheduler();
   return 0;
 }
