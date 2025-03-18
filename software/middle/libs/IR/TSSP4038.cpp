20dbdb8254e5fd3f4e7bab4528bfb62308dcf52f
jingyi679 <j29269428@gmail.com>
2025-03-18 18:17:17 +0800

    10% through building

--- software/middle/libs/IR/TSSP4038.cpp
+++ software/middle/libs/IR/TSSP4038.cpp
@@ -1,41 +1,12 @@
 #include "TSSP4038.hpp"
 #include "include/TSSP4038.hpp"
-extern "C"{
+extern "C" {
 #include "hardware/gpio.h"
 }
 #include "types.hpp"
 #include "comms.hpp"
 #include "pinmap.hpp"
 
-const int freq = 40000; 
-const int period = 1000000 / freq; 
-const int waveform_freq = 1200; //mod frequency
-const float samples_per_window = freq / waveform_freq;
-
-volatile int mod_step = 0; //keep track of which mod step we are on
-struct repeating_timer modulation_timer;
-
-//GPIO for array of pins :D
-const int ir_pins[] = {
-    pinmap::Pico::IR1, pinmap::Pico::IR2, pinmap::Pico::IR3, pinmap::Pico::IR4,
-    pinmap::Pico::IR5, pinmap::Pico::IR6, pinmap::Pico::IR7, pinmap::Pico::IR8,
-    pinmap::Pico::IR9, pinmap::Pico::IR10, pinmap::Pico::IR11, pinmap::Pico::IR12,
-    pinmap::Pico::IR13, pinmap::Pico::IR14, pinmap::Pico::IR15, pinmap::Pico::IR16,
-    pinmap::Pico::IR17, pinmap::Pico::IR18, pinmap::Pico::IR19, pinmap::Pico::IR20,
-    pinmap::Pico::IR21, pinmap::Pico::IR22, pinmap::Pico::IR23, pinmap::Pico::IR24
-};
-
-const int num_ir_sensors = sizeof(ir_pins) / sizeof(ir_pins[0]);
-
-// Create array of Samples obj, each IR sensor??
-IRSensor* ir_samples[num_ir_sensors];
-
-//rising edge and falling edge timestamps
-volatile uint32_t rising_time[num_ir_sensors] = {0};
-volatile uint32_t falling_time[num_ir_sensors] = {0};
-
-//mod steps
-const float mod_duty_cycle[5] = {1.0, 0.25, 0.0625, 0.015625, 0.0}
 
 IRSensor::IRSensor(int n_samples) { 
     this->n_samples = n_samples; 
@@ -67,10 +38,10 @@ static bool IRSensor::modulation_timer_callback(struct repeating_timer *t) {
 }
 
 //interrupt when rising edge detected
-static void IRSensor::rising_edge(unit gpio, uint32_t events) { 
+void IRSensor::rising_edge(uint8_t gpio, uint32_t events) { 
     for (int i = 0; i<num_ir_sensors; i++) { 
         if (gpio == ir_pins[i]) { 
-            ir_samples[i]->add(true); 
+            n_samples[i]->add(true); 
             rising_time[i] = time_us_32();
             return; 
         } 
@@ -79,10 +50,10 @@ static void IRSensor::rising_edge(unit gpio, uint32_t events) {
 }
 
 //interrupt when falling edge detected
-static void IRSensor::falling_edge(unit gpio, uint32_t events) { 
+void IRSensor::falling_edge(uint8_t gpio, uint32_t events) { 
     for (int i = 0; i<num_ir_sensors; i++) { 
         if (gpio == ir_pins[i]) { 
-            ir_samples[i]->add(false); 
+            n_samples[i]->add(false); 
             falling_time[i] = time_us_32();
             return; 
         } 
--- software/middle/libs/IR/include/TSSP4038.hpp
+++ software/middle/libs/IR/include/TSSP4038.hpp
@@ -5,19 +5,37 @@ extern "C" {
     #include <hardware/gpio.h>
 } 
 #include "types.hpp"
-#include "pins/MCP23S17.hpp"
+#include "pinmap.hpp"
 #define IR_SENSOR_HPP
 
-const int ir_pins[];
-const int num_ir_sensors;
-const int freq;
-const int period;
-const int waveform_freq;
-const float samples_per_window;
-volatile int mod_step = 0;
-struct repeating_timer modulation_timer;
-Samples* ir_samples[num_ir_sensors];
+const int freq = 40000; 
+const int period = 1000000 / freq; 
+const int waveform_freq = 1200; //mod frequency
+const float samples_per_window = freq / waveform_freq;
 
+volatile int mod_step = 0; //keep track of which mod step we are on
+struct repeating_timer modulation_timer;
+
+const int num_ir_sensors = sizeof(ir_pins) / sizeof(ir_pins[0]);
+
+// Create array of Samples obj, each IR sensor??
+IRSensor* ir_samples[num_ir_sensors];
+
+//rising edge and falling edge timestamps
+volatile uint32_t rising_time[num_ir_sensors] = {0};
+volatile uint32_t falling_time[num_ir_sensors] = {0};
+
+//mod steps
+const float mod_duty_cycle[5] = {1.0, 0.25, 0.0625, 0.015625, 0.0};
+
+const int ir_pins[] = {
+    (int)pinmap::Pico::IR1, (int)pinmap::Pico::IR2, (int)pinmap::Pico::IR3, (int)pinmap::Pico::IR4,
+    (int)pinmap::Pico::IR5, (int)pinmap::Pico::IR6, (int)pinmap::Pico::IR7, (int)pinmap::Pico::IR8,
+    (int)pinmap::Pico::IR9, (int)pinmap::Pico::IR10, (int)pinmap::Pico::IR11, (int)pinmap::Pico::IR12,
+    (int)pinmap::Pico::IR13, (int)pinmap::Pico::IR14, (int)pinmap::Pico::IR15, (int)pinmap::Pico::IR16,
+    (int)pinmap::Pico::IR17, (int)pinmap::Pico::IR18, (int)pinmap::Pico::IR19, (int)pinmap::Pico::IR20,
+    (int)pinmap::Pico::IR21, (int)pinmap::Pico::IR22, (int)pinmap::Pico::IR23, (int)pinmap::Pico::IR24
+};
 
 class IRSensor {
     public:
@@ -25,8 +43,8 @@ class IRSensor {
         void add(bool state);
         float average(void);
         static bool modulation_timer_callback(struct repeating_timer *t);
-        static void rising_edge(uint gpio, uint32_t events);
-        static void falling_edge(uint gpio, uint32_t events);
+        void rising_edge(uint8_t gpio, uint32_t events);
+        void falling_edge(uint8_t gpio, uint32_t events);
         void init();
         void read_raw();
     private:
