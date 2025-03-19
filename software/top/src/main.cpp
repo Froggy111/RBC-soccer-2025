8c70f2981b24d138f80dd2fe5d56e9b1db79cbce
AJR07 <angjunray@live.com>
2025-03-19 11:14:53 +0800

    LEDs for top plate :O

--- software/top/src/main.cpp
+++ software/top/src/main.cpp
@@ -1,3 +1,38 @@
-int main() {
+#include "comms/usb.hpp"
 
+extern "C" {
+#include <pico/stdlib.h>
+#include <hardware/spi.h>
+#include <hardware/i2c.h>
+}
+#include "comms.hpp"
+
+const int LED_PIN = 25;
+
+int main() {
+  // * Init LED
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_put(LED_PIN, 1);
+
+  // * Init USB Comms
+  comms::USB_CDC.init();
+
+  // * Init SPIs
+  if (!spi_init(spi0, 1000000)) {
+    comms::USB_CDC.printf("SPI Initialized\n");
+    return -1;
+  } else {
+    comms::USB_CDC.printf("SPI\n");
+  }
+
+  // * Init I2C
+  if (!i2c_init(i2c0, 100000)) {
+    comms::USB_CDC.printf("I2C Initialized\n");
+    return -1;
+  } else {
+    comms::USB_CDC.printf("I2C\n");
+  }
+
+  vTaskStartScheduler();
 }
---
+++ software/top/src/sensors/IMUs.cpp
@@ -0,0 +1,7 @@
+
+
+MouseSensor mouse_sensor_1, mouse_sensor_2;
+
+void line_sensor_task(void *args) {
+
+}
