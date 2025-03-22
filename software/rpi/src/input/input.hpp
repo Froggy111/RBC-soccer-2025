6511eeba674115915615a09a696ff28a33c57242
AJR07 <angjunray@live.com>
2025-03-22 22:40:33 +0800

    :fire: Remove src

--- software/rpi/src/input/input.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "main/state_input.hpp"
-#include "utils/fps.hpp"
-
-void InputLoop(InputStateManager& input_state_manager, FPS& fps_manager);
--- software/rpi/src/types/loop_types.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum LoopType {
-  INPUT,
-  STRATEGY,
-  OUTPUT
-}; 
