a5d799e577399025dbd4afc11dcb490e24d67805
AJR07 <angjunray@live.com>
2025-03-23 22:37:42 +0800

    :bug: Fix randomnization bugs

--- software/rpi/libs/camera/processor.cpp
+++ software/rpi/libs/camera/processor.cpp
@@ -7,24 +7,30 @@
 #include <tuple>
 
 namespace camera {
-std::tuple<std::pair<Pos, float>, std::pair<Pos, float>, std::pair<Pos, float>,
-           std::pair<Pos, float>>
-CamProcessor::get_points(const cv::Mat &frame) {
-    // find minima from all 4 corners
-    Pos top_left(5, 5);
-    Pos top_right(frame.cols - 5, 5);
-    Pos bottom_left(5, frame.rows - 5);
-    Pos bottom_right(frame.cols - 5, frame.rows - 5);
-
-    // find minima from all 4 corners
-    std::pair<Pos, float> top_left_res     = find_minima(frame, top_left);
-    std::pair<Pos, float> top_right_res    = find_minima(frame, top_right);
-    std::pair<Pos, float> bottom_left_res  = find_minima(frame, bottom_left);
-    std::pair<Pos, float> bottom_right_res = find_minima(frame, bottom_right);
-
-    return std::make_tuple(top_left_res, top_right_res, bottom_left_res,
-                           bottom_right_res);
+inline int generate_random_number(int mid, int variance) {
+    // Generate a random number between mid - variance and mid + variance
+    int random_number = mid - variance + (rand() % (2 * variance));
+    return random_number;
 }
+
+// std::tuple<std::pair<Pos, float>, std::pair<Pos, float>, std::pair<Pos, float>,
+//            std::pair<Pos, float>>
+// CamProcessor::get_points(const cv::Mat &frame) {
+//     // find minima from all 4 corners
+//     Pos top_left(5, 5);
+//     Pos top_right(frame.cols - 5, 5);
+//     Pos bottom_left(5, frame.rows - 5);
+//     Pos bottom_right(frame.cols - 5, frame.rows - 5);
+
+//     // find minima from all 4 corners
+//     std::pair<Pos, float> top_left_res     = find_minima(frame, top_left);
+//     std::pair<Pos, float> top_right_res    = find_minima(frame, top_right);
+//     std::pair<Pos, float> bottom_left_res  = find_minima(frame, bottom_left);
+//     std::pair<Pos, float> bottom_right_res = find_minima(frame, bottom_right);
+
+//     return std::make_tuple(top_left_res, top_right_res, bottom_left_res,
+//                            bottom_right_res);
+// }
 
 float CamProcessor::calculate_loss(const cv::Mat &camera_image, Pos &guess) {
     uint32_t count = 0, non_white = 0;
@@ -66,14 +72,6 @@ float CamProcessor::calculate_loss(const cv::Mat &camera_image, Pos &guess) {
         count += 1;
     }
 
-    // if (guess.heading != 0 && count == 0) {
-    //     printf("Found none.\n");
-    // }
-
-    // if (guess.heading != 0 && count != 0) {
-    //     printf("Found one.\n");
-    // }
-
     if (count == 0) {
         return 1.0f;
     }
@@ -86,24 +84,37 @@ std::pair<Pos, float> CamProcessor::find_minima(const cv::Mat &camera_image,
                                                 Pos &initial_guess) {
     Pos best_guess  = initial_guess;
     float best_loss = calculate_loss(camera_image, best_guess);
-    for (int i = 0; i < NUM_GENERATIONS; i++) {
+    while (best_loss > 0.3f) {
         Pos current_best_guess  = best_guess;
         float current_best_loss = best_loss;
+        printf("Best Loss: %f %d %d\n", best_loss, best_guess.x, best_guess.y);
 
         // disperse points x times
         for (int j = 0; j < NUM_PARTICLES_PER_GENERATION; j++) {
-            // randomize new guess properties, with the randomness proportional to the best_loss
-            // ensuring that the new guesses are within the bounds of the image (0, camera_image.cols), (0, camera_image.rows)
             Pos new_guess = best_guess;
 
-            // TODO: Ensure that the new guess is within the bounds of the image
-            new_guess.x += (rand() % (int)(best_loss * 2 * camera_image.cols)) -
-                           (best_loss * camera_image.cols);
-            new_guess.y += (rand() % (int)(best_loss * 2 * camera_image.rows)) -
-                           (best_loss * camera_image.rows);
-            new_guess.heading +=
-                (rand() % (int)(best_loss * M_PI)) - (best_loss * M_PI / 2);
-            
+            // Ensure that the new guess is within the bounds of the image
+            // Done by restricting the x and y coords
+            if (new_guess.x < (best_loss * FIELD_WIDTH) / 2) {
+                new_guess.x = (best_loss * FIELD_WIDTH) / 2;
+            } else if (new_guess.x >= FIELD_WIDTH - (best_loss * FIELD_WIDTH) / 2) {
+                new_guess.x = FIELD_WIDTH - (best_loss * FIELD_WIDTH) / 2;
+            }
+
+            if (new_guess.y < (best_loss * FIELD_HEIGHT) / 2) {
+                new_guess.y = (best_loss * FIELD_HEIGHT) / 2;
+            } else if (new_guess.y >= FIELD_HEIGHT - (best_loss * FIELD_HEIGHT) / 2) {
+                new_guess.y = FIELD_HEIGHT - (best_loss * FIELD_HEIGHT) / 2;
+            }
+
+            // randomize new guess properties, with the randomness proportional to the best_loss
+            new_guess.x =
+                generate_random_number(new_guess.x, best_loss * FIELD_WIDTH);
+            new_guess.y =
+                generate_random_number(new_guess.y, best_loss * FIELD_HEIGHT);
+            new_guess.heading =
+                generate_random_number(new_guess.heading, best_loss * M_PI * 2);
+
             // calculate loss
             float new_loss = calculate_loss(camera_image, new_guess);
             if (new_loss < current_best_loss) {
