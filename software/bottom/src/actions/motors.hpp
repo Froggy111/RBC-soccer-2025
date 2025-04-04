29b77cca0a2c99cc15313248416ed3dde58fad4f
Froggy111 <lijiayi4789@gmail.com>
2025-04-04 14:27:43 +0800

    it works now????

--- software/bottom/src/actions/motors.hpp
+++ software/bottom/src/actions/motors.hpp
@@ -24,93 +24,133 @@ static MotorRecvData motor_task_data = {};
 static u8 motor_task_buffer[sizeof(motor_task_data)];
 static SemaphoreHandle_t motor_data_mutex = nullptr;
 
-static i16 current_duty_cycles[4] = {0};
-static i16 target_duty_cycles[4] = {0};
-const u32 max_duty_cycle = 12500;
-const u32 min_duty_cycle_ramp = 10000;
-const u32 max_duty_cycle_per_ms = max_duty_cycle / min_duty_cycle_ramp;
+// static i16 current_duty_cycles[4] = {0};
+// static i16 target_duty_cycles[4] = {0};
+// const u32 max_duty_cycle = 12500;
+// const u32 min_duty_cycle_ramp = 10000;
+// const u32 max_duty_cycle_per_ms = max_duty_cycle / min_duty_cycle_ramp;
 
 void motor_task(void *args) {
   // initialize all motors
   if (driver1.init(1, spi0)) {
-    debug::info("Motor Driver 1 Initialized!\n");
+    debug::info("Motor Driver 1 Initialized!\r\n");
   } else {
-    debug::error("Motor Driver 1 Initialization Failed!\n");
+    debug::error("Motor Driver 1 Initialization Failed!\r\n");
     while (true) {
       vTaskDelay(pdMS_TO_TICKS(1000));
     }
   }
-  vTaskDelay(pdMS_TO_TICKS(1));
 
   if (driver2.init(2, spi0)) {
-    debug::info("Motor Driver 2 Initialized!\n");
+    debug::info("Motor Driver 2 Initialized!\r\n");
   } else {
-    debug::error("Motor Driver 2 Initialization Failed!\n");
+    debug::error("Motor Driver 2 Initialization Failed!\r\n");
     while (true) {
       vTaskDelay(pdMS_TO_TICKS(1000));
     }
   }
-  vTaskDelay(pdMS_TO_TICKS(1));
 
   if (driver3.init(3, spi0)) {
-    debug::info("Motor Driver 3 Initialized!\n");
+    debug::info("Motor Driver 3 Initialized!\r\n");
   } else {
-    debug::error("Motor Driver 3 Initialization Failed!\n");
+    debug::error("Motor Driver 3 Initialization Failed!\r\n");
     while (true) {
       vTaskDelay(pdMS_TO_TICKS(1000));
     }
   }
-  vTaskDelay(pdMS_TO_TICKS(1));
 
   if (driver4.init(4, spi0)) {
-    debug::info("Motor Driver 4 Initialized!\n");
+    debug::info("Motor Driver 4 Initialized!\r\n");
   } else {
-    debug::error("Motor Driver 4 Initialization Failed!\n");
+    debug::error("Motor Driver 4 Initialization Failed!\r\n");
     while (true) {
       vTaskDelay(pdMS_TO_TICKS(1000));
     }
   }
-  vTaskDelay(pdMS_TO_TICKS(1));
 
   debug::info("Motors initialized");
 
-  vTaskDelay(10000);
   TickType_t previous_wait_time = xTaskGetTickCount();
   for (;;) {
-    vTaskDelayUntil(&previous_wait_time, pdMS_TO_TICKS(1));
-    previous_wait_time = xTaskGetTickCount();
+    // for (int i = 0; i <= 650; i++) {
+    //   driver1.command(i * 10);
+    //   driver2.command(i * 10);
+    //   driver3.command(i * 10);
+    //   driver4.command(i * 10);
+    //
+    //   // debug::log"Current: %d %d %d %d\n", driver1.read_current(), driver2.read_current(),
+    //   //        driver3.read_current(), driver4.read_current());
+    //
+    //   vTaskDelay(pdMS_TO_TICKS(10));
+    // }
+    //
+    // vTaskDelay(pdMS_TO_TICKS(10));
+    // for (int i = 650; i >= 0; i--) {
+    //   driver1.command(i * 10);
+    //   driver2.command(i * 10);
+    //   driver3.command(i * 10);
+    //   driver4.command(i * 10);
+    //
+    //   // debug::log"Current: %d %d %d %d\n", driver1.read_current(), driver2.read_current(),
+    //   //        driver3.read_current(), driver4.read_current());
+    //
+    //   vTaskDelay(pdMS_TO_TICKS(10));
+    // }
+    // vTaskDelay(pdMS_TO_TICKS(10));
+    // vTaskDelayUntil(&previous_wait_time, pdMS_TO_TICKS(1));
+    // previous_wait_time = xTaskGetTickCount();
     // * data transfer
-    if (ulTaskNotifyTake(pdTRUE, 0)) {
-      xSemaphoreTake(motor_data_mutex, portMAX_DELAY);
-      memcpy(&motor_task_data, motor_task_buffer, sizeof(motor_task_data));
-      memset(motor_task_buffer, 0, sizeof(motor_task_buffer));
-      xSemaphoreGive(motor_data_mutex);
-      target_duty_cycles[motor_task_data.id + 1] = motor_task_data.duty_cycle;
+
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    xSemaphoreTake(motor_data_mutex, portMAX_DELAY);
+    memcpy(&motor_task_data, motor_task_buffer, sizeof(motor_task_data));
+    memset(motor_task_buffer, 0, sizeof(motor_task_buffer));
+    xSemaphoreGive(motor_data_mutex);
+    debug::info("Motor %d: %d\n", motor_task_data.id,
+                motor_task_data.duty_cycle);
+    switch (motor_task_data.id) {
+    case 1:
+      driver1.command(motor_task_data.duty_cycle);
+      break;
+    case 2:
+      driver2.command(motor_task_data.duty_cycle);
+      break;
+    case 3:
+      driver3.command(motor_task_data.duty_cycle);
+      break;
+    case 4:
+      driver4.command(motor_task_data.duty_cycle);
+      break;
+    default:
+      debug::error("Invalid motor ID: %d\n", motor_task_data.id);
+      break;
     }
-    for (u8 id = 0; id < 4; id++) {
-      if (target_duty_cycles[id] != current_duty_cycles[id]) {
-        if (target_duty_cycles[id] - current_duty_cycles[id] > 0) {
-          current_duty_cycles[id] +=
-              MIN(max_duty_cycle_per_ms,
-                  target_duty_cycles[id] - current_duty_cycles[id]);
-        } else {
-          current_duty_cycles[id] -=
-              MIN(max_duty_cycle_per_ms,
-                  current_duty_cycles[id] - target_duty_cycles[id]);
-        }
-        switch (id) {
-        case 1:
-          driver1.command(current_duty_cycles[id]);
-        case 2:
-          driver2.command(current_duty_cycles[id]);
-        case 3:
-          driver3.command(current_duty_cycles[id]);
-        case 4:
-          driver4.command(current_duty_cycles[id]);
-        }
-      }
-    }
+
     // debug::info("Motor %d: %d\n", motor_task_data.id,
-    // motor_task_data.duty_cycle);
+
+    // motor_task_data.duty_cycle);               motor_task_data.duty_cycle);
+    // for (u8 id = 0; id < 4; id++) {
+    //   if (target_duty_cycles[id] != current_duty_cycles[id]) {
+    //     if (target_duty_cycles[id] - current_duty_cycles[id] > 0) {
+    //       current_duty_cycles[id] +=
+    //           MIN(max_duty_cycle_per_ms,
+    //               target_duty_cycles[id] - current_duty_cycles[id]);
+    //     } else {
+    //       current_duty_cycles[id] -=
+    //           MIN(max_duty_cycle_per_ms,
+    //               current_duty_cycles[id] - target_duty_cycles[id]);
+    //     }
+    //     switch (id) {
+    //     case 1:
+    //       driver1.command(current_duty_cycles[id]);
+    //     case 2:
+    //       driver2.command(current_duty_cycles[id]);
+    //     case 3:
+    //       driver3.command(current_duty_cycles[id]);
+    //     case 4:
+    //       driver4.command(current_duty_cycles[id]);
+    //     }
+    //   }
+    // }
   }
 }
